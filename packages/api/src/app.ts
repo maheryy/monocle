776b8c4aaf28294ae9b44ab43fb4ad1c79facbe4
3kezoh <3kezoh@gmail.com>
2023-07-08 19:48:20 +0200

    feat(api): add compression and helmet

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -3,11 +3,17 @@ import metricRouter from "./metric/metric.router";
 import eventRouter from "./event/event.router";
 import dimensionRouter from "./dimension/dimension.router";
 import docsRouter from "./docs/docs.router";
+import compression from "compression";
+import helmet from "helmet";
 
 export function createMonocleServer() {
   const app = express();
 
   app.use(express.json());
+
+  app.use(compression());
+
+  app.use(helmet());
 
   app.use("/docs", docsRouter);
 
