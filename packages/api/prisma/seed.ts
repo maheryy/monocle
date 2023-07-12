92bc5acc18b8e409ca62293a9471f40ac087ffd0
3kezoh <3kezoh@gmail.com>
2023-07-12 14:40:58 +0200

    chore(api): update seeds

--- packages/api/prisma/seed.ts
+++ packages/api/prisma/seed.ts
@@ -1,9 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { faker } from "@faker-js/faker";
+import { faker, he } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
 async function main() {
+  await Promise.all([
+    prisma.event.deleteMany({}),
+    prisma.metric.deleteMany({}),
+    prisma.dimension.deleteMany({}),
+  ]);
+
   const eventNames = [
     "Product added to cart",
     "Product removed from cart",
@@ -11,7 +17,7 @@ async function main() {
     "Product reviewed",
   ];
 
-  const events = Array.from({ length: 50 }).map(() => ({
+  const customEvents = Array.from({ length: 50 }).map(() => ({
     name: faker.helpers.arrayElement(eventNames),
     app: "react",
     userId: faker.string.uuid(),
@@ -21,38 +27,75 @@ async function main() {
     },
   }));
 
-  const metricNames = [
-    "Time to first paint",
-    "Time to first contentful paint",
-    "Time to first meaningful paint",
-    "Time to interactive",
-    "Total blocking time",
-    "Cumulative layout shift",
-  ];
+  const mouseEvents = Array.from({ length: 25 }).map(() => {
+    const width = 1920;
+    const height = 1080;
 
-  const metrics = Array.from({ length: 50 }).map(() => ({
-    name: faker.helpers.arrayElement(metricNames),
-    app: "react",
-    userId: faker.string.uuid(),
-    value: faker.number.int(3000),
-  }));
+    const length = faker.number.int({ min: 1, max: 1000 });
 
-  const userAgents = Array.from({ length: 50 }).map(() => ({
-    name: "User-Agent",
+    const mousePositions = Array.from({ length }).map((_, index) => ({
+      clientX: faker.number.int({ min: 0, max: width }),
+      clientY: faker.number.int({ min: 0, max: height }),
+      pageX: faker.number.int({ min: 0, max: width }),
+      pageY: faker.number.int({ min: 0, max: height }),
+      timeStamp: index * 100,
+    }));
+
+    return {
+      name: "mouse",
+      app: "react",
+      userId: faker.string.uuid(),
+      payload: {
+        mousePositions,
+        page: faker.internet.url(),
+      },
+    };
+  });
+
+  const webVitals = {
+    CLS: [0, 0.5, "float"] as const,
+    LCP: [0, 8000, "int"] as const,
+    FID: [0, 600, "int"] as const,
+    FCP: [0, 6000, "int"] as const,
+    TTFB: [0, 2600, "int"] as const,
+    INP: [0, 1000, "int"] as const,
+  };
+
+  const webVitalMetrics = Array.from({ length: 50 }).map(() => {
+    const [name, value] = faker.helpers.objectEntry(webVitals);
+    const [min, max, kind] = value;
+
+    return {
+      name,
+      app: "react",
+      userId: faker.string.uuid(),
+      value: faker.number[kind]({ min, max }),
+    };
+  });
+
+  const userAgentDimensions = Array.from({ length: 50 }).map(() => ({
+    name: "user-agent",
     app: "react",
     userId: faker.string.uuid(),
     value: faker.internet.userAgent(),
   }));
 
+  const pageViewDimensions = Array.from({ length: 50 }).map(() => ({
+    name: "page-view",
+    app: "react",
+    userId: faker.string.uuid(),
+    value: faker.internet.url(),
+  }));
+
   await Promise.all([
     prisma.event.createMany({
-      data: events,
+      data: [...customEvents, ...mouseEvents],
     }),
     prisma.metric.createMany({
-      data: metrics,
+      data: webVitalMetrics,
     }),
     prisma.dimension.createMany({
-      data: userAgents,
+      data: [...userAgentDimensions, ...pageViewDimensions],
     }),
   ]);
 }
