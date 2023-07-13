f7d777d24ec93b986abefa6ea1f0f5794c8b0344
3kezoh <3kezoh@gmail.com>
2023-07-13 13:59:51 +0200

    feat(api): add container configuration via env

--- packages/api/docker/index.ts
+++ packages/api/docker/index.ts
@@ -1,7 +1,23 @@
 import { createMonocleServer } from "../src";
 
-const server = createMonocleServer();
+const { MONOCLE_DATABASE_URL, CORS_ORIGIN } = process.env;
+
+if (!MONOCLE_DATABASE_URL) {
+  throw new Error("Missing MONOCLE_DATABASE_URL");
+}
+
+if (!CORS_ORIGIN) {
+  console.warn("No CORS_ORIGIN provided, defaulting to *");
+}
+
+const origin = CORS_ORIGIN || "*";
+
+const server = createMonocleServer({
+  corsOptions: {
+    origin,
+  },
+});
 
 server.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log("Monocle API server listening on port", 3000);
 });
