a9b1bbf9f5512694fd040a46390f0fe74d2c9e86
Prado <mahery.rsh@gmail.com>
2023-07-15 14:34:42 +0200

    fix: temporary solution for cors issues with beacon

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,7 +1,15 @@
 import { createMonocleServer } from "@ekezoh/monocle.api";
 
 const server = createMonocleServer({
-  corsOptions: { origin: "*" },
+  corsOptions: {
+    origin: [
+      "http://localhost:8080",
+      "http://localhost:8081",
+      "http://localhost:3000",
+      "http://localhost:3001",
+    ],
+    credentials: true,
+  },
 });
 
 server.listen(3000);
